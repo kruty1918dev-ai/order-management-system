aed91285e5983ccd3c6e9b18880091e7bc4b01b7
Kruty1918 <kruty1918.dev@gmail.com>
2026-04-20 22:00:54 +0200

    update

--- tests/OrderManagementSystem.Tests/obj/Debug/net10.0/OrderManagementSystem.Tests.AssemblyInfo.cs
+++ tests/OrderManagementSystem.Tests/obj/Debug/net10.0/OrderManagementSystem.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+286210fd599973a4f529ae97a096f019a3f104a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac987214e0fbe4f60edfbddbba6b8824ef199c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
