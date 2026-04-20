286210fd599973a4f529ae97a096f019a3f104a1
Kruty1918 <kruty1918.dev@gmail.com>
2026-04-20 15:21:05 +0200

    update

--- obj/Debug/net10.0/order-management-system.AssemblyInfo.cs
+++ obj/Debug/net10.0/order-management-system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("order-management-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbae8e330df2044649a0db193fe153f14f55bad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("order-management-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("order-management-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
